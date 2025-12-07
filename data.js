3f287dabc9825edee10e13b0f6a01aa40ceaf8ee
TimeZonax <68100248@kmitl.ac.th>
2025-12-07 18:07:42 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const shopsData = [
+    {name:'ร้าน A', menu:[{name:'เมนู A1', price:50},{name:'เมนู A2', price:70},{name:'เมนู A3', price:60}]},
+    {name:'ร้าน B', menu:[{name:'เมนู B1', price:80},{name:'เมนู B2', price:60},{name:'เมนู B3', price:90}]},
+    {name:'ร้าน C', menu:[{name:'เมนู C1', price:40},{name:'เมนู C2', price:60},{name:'เมนู C3', price:50}]},
+    {name:'ร้าน D', menu:[{name:'เมนู D1', price:55},{name:'เมนู D2', price:65},{name:'เมนู D3', price:75}]},
+    {name:'ร้าน E', menu:[{name:'เมนู E1', price:30},{name:'เมนู E2', price:50},{name:'เมนู E3', price:40}]}
+];
