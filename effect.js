07b8a9c2eb9b4c929e0b1160f6c88b13e781daaa
TimeZonax <68100248@kmitl.ac.th>
2025-12-07 19:14:33 +0700

    Update and rename effect.js to garden.html

--- effect.js
+++
@@ -1,8 +0,0 @@
-function buttonEffect(button){
-    button.classList.add('active');
-    setTimeout(()=> button.classList.remove('active'),200);
-}
-
-document.querySelectorAll('.glow-button').forEach(btn=>{
-    btn.addEventListener('click', ()=> buttonEffect(btn));
-});
